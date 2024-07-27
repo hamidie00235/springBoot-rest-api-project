6d36d99d0e35b40e777bb1deb1c593fffeec18e5
hamidie ahmat <ahmathisseinh@gmail.com>
2024-07-27 15:35:27 +0300

    Spring Boot REST API with Path Variable - @PathVariable

--- src/main/java/net/hamdev/springBoot/controller/StudentController.java
+++ src/main/java/net/hamdev/springBoot/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.hamdev.springBoot.controller;
 
 import net.hamdev.springBoot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class StudentController {
         );
                 return student;
     }
+    //
     // http://localhost:8080/students
     @GetMapping("students")
     public List<Student> getStudents(){
@@ -30,5 +32,15 @@ public class StudentController {
         students.add(new Student(3,"omar","yusuf"));
         students.add(new Student(4,"yakup","bachar"));
         return students;
+    }
+    // spring Boot rest api with path Variable
+    // {id} url template variable
+    // http://localhost:8080/students/1/hamidie/ahmat
+
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable( @PathVariable("id") int studentID ,
+            @PathVariable("first-name") String firstName,
+           @PathVariable("last-name") String lastName){
+        return new Student(studentID,firstName,lastName);
     }
 }
